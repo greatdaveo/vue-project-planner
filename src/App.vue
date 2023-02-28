1c502d447727c0c12aaa0022dbe5c34189671eaa
David Olowomeye <olowomeyedavid242@gmail.com>
2023-02-28 13:17:35 +0100

    Implemented the delete function

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+body {
+  background: #f2f2f2;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #555;
+  margin: 0 auto;
+  max-width: 600px;
+}
+</style>
